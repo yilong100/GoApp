a815f0990f4808d09d119ba30a8f2e8a91cac35b
James Wu <james.wu@fdmgroup.com>
2023-10-09 12:41:01 +1300

    added code for routes.go

--- backend/server/routes.go
+++ backend/server/routes.go
@@ -2,10 +2,12 @@ package server
 
 import (
 	"github.com/gorilla/mux"
+	"net/http"
+	"github.com/yourusername/your_project/handlers"
 )
 
-func NewRouter() *mux.Router {
-	router := mux.NewRouter()
-	routes(router)
-	return router
+func routes(router *mux.Router) {
+	router.HandleFunc("/users", handlers.GetUsers).Methods("GET")
+	router.HandleFunc("/users/{id:[0-9]+}", handlers.GetUserByID).Methods("GET")
+	router.HandleFunc("/users", handlers.CreateUser).Methods("POST")
 }
