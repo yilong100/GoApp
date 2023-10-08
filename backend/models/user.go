b766ef8418c08973f344cf6913288be24cccab85
James Wu <james.wu@fdmgroup.com>
2023-10-09 12:45:19 +1300

    added code for user.go

---
+++ backend/models/user.go
@@ -0,0 +1,6 @@
+type user struct {
+	ID               string `json: id`
+	Name             string `json: "name"`
+	Age              int    `json: "age"`
+	DreamPlaceToLive string `json: "dreamplacetolive`
+}
