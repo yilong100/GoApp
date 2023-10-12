47e7501bc167531e18d7ac1bff6a6857486d6b5c
James Wu <james.wu@fdmgroup.com>
2023-10-12 13:34:58 +1300

    added terraform folder and code that creates a VM

---
+++ terraform/createVM/main.tf
@@ -0,0 +1,25 @@
+# This block defines a Google Compute Engine instance resource with the name "vm2-from-terraform"
+
+resource "google_compute_instance" "vm-from-terraform" {
+  name         = "vm2-from-terraform"
+  machine_type = "e2-medium"
+  zone         = "australia-southeast1-a"
+
+  # Block that defines the boot disk for the VM
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11" # Specifies the boot image
+      labels = {
+        my_label = "value" # Labels applied to the boot disk
+      }
+    }
+  }
+
+  # Block that defines the network interface for the VM
+  network_interface {
+    network = "default" # Specifies the network to which the VM is connected.
+  }
+
+  metadata_startup_script = "echo hi > /test.txt" # Specifies a startup script for the VM
+  # allow_stopping_for_update = true # This line is commented out but can be used to allow VM stopping during updates
+}
---
+++ terraform/createVM/provider.tf
@@ -0,0 +1,20 @@
+# This section specifies the required cloud provider and its source and version
+# Terraform will ensure that the specified cloud provider is available for use
+
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google" # Source for the Google Cloud provider
+      version = "5.1.0"            # Version of the Google Cloud provider
+    }
+  }
+}
+
+# change everytime
+# This section configures the Google Cloud provider for your infrastructure
+provider "google" {
+  project     = "playground-s-11-fdeff281" # Your Google Cloud Project ID
+  region      = "australia-southeast1"     # The desired region for resources
+  zone        = "australia-southeast1-a"   # The specific zone within the region
+  credentials = "./keys.json"              # Path to your service account credentials file
+}
