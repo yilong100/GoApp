0554786bf7b5fa70e24c34d1e1975129e364d1c0
James Wu <james.wu@fdmgroup.com>
2023-10-18 15:16:56 +1300

    changed go version to 1.21 to test out connecting app to postgres db

--- backend/db/user_db.go
+++ backend/db/user_db.go
@@ -1,104 +1,67 @@
 package db
 
-import (
-	"context"
-	"example/GoPractice/models"
-	"fmt"
+// mongodb local connection, get, insert data
 
-	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
-)
-
-// func AddToMongoDBDatabase(userToInsert *models.User) {
+// func ConnectToMongoDB() (*mongo.Client, error) {
 // 	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
 // 	client, err := mongo.Connect(context.TODO(), clientOptions)
 // 	if err != nil {
-// 		log.Fatal(err)
+// 		return nil, err
 // 	}
-// 	defer client.Disconnect(context.TODO())
 
 // 	// Check the connection
 // 	err = client.Ping(context.TODO(), nil)
 // 	if err != nil {
-// 		log.Fatal(err)
+// 		return nil, err
 // 	}
 
 // 	fmt.Println("Connected to MongoDB!")
+// 	return client, nil
+// }
 
+// func GetAllData(client *mongo.Client, databaseName, collectionName string) ([]bson.M, error) {
+// 	// Access the specified database and collection
+// 	database := client.Database(databaseName)
+// 	collection := database.Collection(collectionName)
+
+// 	// Define an empty filter to match all documents
+// 	filter := bson.M{}
+
+// 	// Execute the query to find all documents
+// 	cursor, err := collection.Find(context.TODO(), filter)
+// 	if err != nil {
+// 		return nil, err
+// 	}
+// 	defer cursor.Close(context.TODO())
+
+// 	var results []bson.M
+
+// 	// Iterate through the results and decode each document into a map
+// 	for cursor.Next(context.TODO()) {
+// 		var result bson.M
+// 		if err := cursor.Decode(&result); err != nil {
+// 			return nil, err
+// 		}
+// 		results = append(results, result)
+// 	}
+
+// 	if err := cursor.Err(); err != nil {
+// 		return nil, err
+// 	}
+
+// 	return results, nil
+// }
+
+// func InsertUserData(client *mongo.Client, userToInsert *models.User) error {
 // 	// Access a specific database and collection
 // 	database := client.Database("mydb")
 // 	collection := database.Collection("mycollection")
 
-// 	_, err = collection.InsertOne(context.TODO(), userToInsert)
+// 	_, err := collection.InsertOne(context.TODO(), userToInsert)
 // 	if err != nil {
-// 		log.Fatal(err)
+// 		return err
 // 	}
 
 // 	fmt.Println("User inserted successfully!")
-
+// 	return nil
 // }
-
-func ConnectToMongoDB() (*mongo.Client, error) {
-	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
-	client, err := mongo.Connect(context.TODO(), clientOptions)
-	if err != nil {
-		return nil, err
-	}
-
-	// Check the connection
-	err = client.Ping(context.TODO(), nil)
-	if err != nil {
-		return nil, err
-	}
-
-	fmt.Println("Connected to MongoDB!")
-	return client, nil
-}
-
-func GetAllData(client *mongo.Client, databaseName, collectionName string) ([]bson.M, error) {
-	// Access the specified database and collection
-	database := client.Database(databaseName)
-	collection := database.Collection(collectionName)
-
-	// Define an empty filter to match all documents
-	filter := bson.M{}
-
-	// Execute the query to find all documents
-	cursor, err := collection.Find(context.TODO(), filter)
-	if err != nil {
-		return nil, err
-	}
-	defer cursor.Close(context.TODO())
-
-	var results []bson.M
-
-	// Iterate through the results and decode each document into a map
-	for cursor.Next(context.TODO()) {
-		var result bson.M
-		if err := cursor.Decode(&result); err != nil {
-			return nil, err
-		}
-		results = append(results, result)
-	}
-
-	if err := cursor.Err(); err != nil {
-		return nil, err
-	}
-
-	return results, nil
-}
-
-func InsertUserData(client *mongo.Client, userToInsert *models.User) error {
-	// Access a specific database and collection
-	database := client.Database("mydb")
-	collection := database.Collection("mycollection")
-
-	_, err := collection.InsertOne(context.TODO(), userToInsert)
-	if err != nil {
-		return err
-	}
-
-	fmt.Println("User inserted successfully!")
-	return nil
-}
