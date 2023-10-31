067e82ae562abacfc3ba6d376f11748a2bfc4903
James Wu <james.wu@fdmgroup.com>
2023-10-31 13:04:05 +1300

    .

--- terraform/createVM/database.tf
+++ terraform/createVM/database.tf
@@ -1,29 +1,29 @@
 resource "google_sql_database_instance" "postgres_goApp" {
-    name             = "go-app-db"
-    database_version = "POSTGRES_13"
-    region           = "australia-southeast1"
-    settings {
-        tier = "db-f1-micro"
+  name             = "go-app-db"
+  database_version = "POSTGRES_13"
+  region           = "australia-southeast1"
+  settings {
+    tier = "db-f1-micro"
 
-        # ip_configuration {
-        #     authorized_networks {
-        #         name            = "my-subnet"
-        #         value           = "10.152.0.0/20"
-        #     }
-        # }
-    }
+    # ip_configuration {
+    #     authorized_networks {
+    #         name            = "my-subnet"
+    #         value           = "10.152.0.0/20"
+    #     }
+    # }
+  }
 
-    deletion_protection = false
+  deletion_protection = false
 
 }
 
 resource "google_sql_user" "user" {
 
-    depends_on = [google_sql_database_instance.postgres_goApp]
+  depends_on = [google_sql_database_instance.postgres_goApp]
 
-    name     = "goApp"
-    instance = google_sql_database_instance.postgres_goApp.name
-    password = "goApp1234"
+  name     = "goApp"
+  instance = google_sql_database_instance.postgres_goApp.name
+  password = "goApp1234"
 }
 
 # Output the assigned IP address
@@ -32,14 +32,14 @@ output "database-ip-address" {
 }
 
 resource "null_resource" "save_database_ip_to_file" {
-    triggers = {
-        instance_id = google_sql_database_instance.postgres_goApp.id
-    }
+  triggers = {
+    instance_id = google_sql_database_instance.postgres_goApp.id
+  }
 
-    depends_on = [google_sql_database_instance.postgres_goApp]
+  depends_on = [google_sql_database_instance.postgres_goApp]
 
-    provisioner "local-exec" {
-        command = "echo ${google_sql_database_instance.postgres_goApp.ip_address[0].ip_address} > ../../backend/db/database-ip-address.txt"
-    }
+  provisioner "local-exec" {
+    command = "echo ${google_sql_database_instance.postgres_goApp.ip_address[0].ip_address} > ../../backend/db/database-ip-address.txt"
+  }
 
 }
